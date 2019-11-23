f15f2409945420d24f8df5a4639d441f541d9be5
jason bartley <jbartley11@gmail.com>
2019-11-23 17:48:52 -0500

    added list of pages, used map to return divs in App component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'JDan Abramov',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
+
 function App() {
   // define variable outside of return
   const helloWorld = 'Welcome to the road to learn react!'
   return (
     <div className="App">
-      <h2>{helloWorld}</h2>
+      {list.map(item => <div key={item.objectID}>
+                          <span>
+                            <a href={item.url}>{item.title}</a>
+                          </span>
+                          <span>{item.author}</span>
+                          <span>{item.num_comments}</span>
+                          <span>{item.points}</span>
+                        </div>)}
     </div>
   );
 }
