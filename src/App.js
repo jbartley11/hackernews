4b2e4500abe3348b427a9dcb326304e63808c815
jason bartley <jbartley11@gmail.com>
2019-11-30 21:18:35 -0500

    fetching from hacker news api

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-]
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
-// const isSearched = searchTerm => item => {
-//   item.title.toLowerCase().includes(searchTerm.toLowerCase());
-// }
+
 // return a function
 const isSearched = searchTerm => item => {
   
@@ -37,12 +20,26 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  setSearchTopStories = result => {
+    this.setState({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   onSearchChange = event => {
@@ -57,7 +54,9 @@ class App extends Component {
   render() {
 
     // destructure state
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null; }
 
     return (
       <div className="page">
@@ -72,7 +71,7 @@ class App extends Component {
           
       
         <Table 
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
